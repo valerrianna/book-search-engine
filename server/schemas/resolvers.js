8e6374b8d74c6759a21527c7ee3f19d966efd753
Valerie <valerie.tram.anh.n@gmail.com>
2022-01-22 16:30:03 -0500

    added resolvers and authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,64 @@
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
       }
-    }
-  };
+      throw new AuthenticationError("Please log in");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
   
+        return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Invalid email");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new AuthenticationError("Invalid password");
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { deletedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
 module.exports = resolvers;
