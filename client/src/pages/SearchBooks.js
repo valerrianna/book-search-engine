7590cb112fa101e466ddb5088a7fd8664c262397
Valerie <valerie.tram.anh.n@gmail.com>
2022-01-23 16:00:22 -0500

    updated searchBook to useMutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,8 +5,8 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
-import { useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
+import { SAVE_BOOK } from "../utils/mutations";
+import { useMutation } from "@apollo/react-hooks";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -16,6 +16,9 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+   // use the Apollo useMutation Hook to execute the SAVE_BOOK mutation
+   const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
